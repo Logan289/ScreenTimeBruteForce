44c60aafe870124946ad76ff850b8a8e5af69d63
thehappydinoa <thehappydinoa@gmail.com>
2017-09-25 11:14:58 -0400

    Update ioscrack.py

--- ioscrack.py
+++ ioscrack.py
@@ -2,33 +2,20 @@
 #Filename: ioscrack.py
 
 from passlib.utils.pbkdf2 import pbkdf2
-import os
-import htos
-import sys
 from time import time
-import base64
+import os, sys, base64
 
-HOMEDIR = '/Users/yuejiadi/Library/Application Support/MobileSync/Backup/'
-PSFILE = '398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b'
-ITERATIONS = 1000
+HOMEDIR = '~/Library/Application Support/MobileSync/Backup/'
 
-backup_dir = os.listdir(HOMEDIR)
-
-for bkup_dir in backup_dir:
-    passfile = open(HOMEDIR + bkup_dir + "/" + PSFILE, "r")
-    line_list = passfile.readlines()
-    secret64 = line_list[6][1:29]
-    salt64 = line_list[10][1:9]
+def crack(secret64, salt64):
     print "secret: ", secret64
     print "salt: ", salt64
-    #secret = htos.to_hex(base64.b64decode(secret64))
     secret = base64.b64decode(secret64)
-    #print secret
     salt = base64.b64decode(salt64) 
     start_t = time()
     for i in range(10000):
         key = "%04d" % ( i )
-        out = pbkdf2(key, salt, ITERATIONS)
+        out = pbkdf2(key, salt, 1000)
         if out == secret:
             print "key: ", key
             duration = time() - start_t
@@ -36,3 +23,17 @@ for bkup_dir in backup_dir:
             sys.exit(0)
     print "no exact key"
 
+try:
+    backup_dir = os.listdir(HOMEDIR)
+    for bkup_dir in backup_dir:
+        passfile = open(HOMEDIR + bkup_dir + "/398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b", "r")
+        line_list = passfile.readlines()
+        secret64 = line_list[6][1:29]
+        salt64 = line_list[10][1:9]
+        crack(secret64, salt64)
+except Exception as e:
+    while not secret64:
+        secret64 = raw_input("Enter Secret Key: ")
+    while not salt64:
+        salt64 = raw_input("Enter Salt: ")
+    crack(secret64, salt64)
