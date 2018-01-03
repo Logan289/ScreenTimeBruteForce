a088e0ef3168d653bb75da013589c83a3a3c1c35
Aidan Holland <thehappydinoa@gmail.com>
2018-01-03 18:42:17 -0500

    Bug fixes

--- ioscrack.py
+++ ioscrack.py
@@ -7,12 +7,12 @@ import os
 import sys
 import base64
 
-HOMEDIR = '~/Library/Application Support/MobileSync/Backup/'
+BACKUPDIR = '~/Library/Application Support/MobileSync/Backup/'
 
 
 def crack(secret64, salt64):
-    print "secret: ", secret64
-    print "salt: ", salt64
+    print("secret: ", secret64)
+    print("salt: ", salt64)
     secret = base64.b64decode(secret64)
     salt = base64.b64decode(salt64)
     start_t = time()
@@ -20,29 +20,32 @@ def crack(secret64, salt64):
         key = "%04d" % (i)
         out = pbkdf2(key, salt, 1000)
         if out == secret:
-            print "key: ", key
+            print("key: ", key)
             duration = time() - start_t
-            print "%f seconds" % (duration)
+            print("%f seconds" % (duration))
             sys.exit(0)
-    print "no exact key"
+    print("no exact key")
 
 
-try:
-    backup_dir = os.listdir(HOMEDIR)
-    for bkup_dir in backup_dir:
-        passfile = open(HOMEDIR + bkup_dir +
-                        "/398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b", "r")
-        line_list = passfile.readlines()
-        secret64 = line_list[6][1:29]
-        salt64 = line_list[10][1:9]
-        crack(secret64, salt64)
-except Exception as e:
-    while not secret64:
+def getHash():
+    try:
+        backup_dir = os.listdir(BACKUPDIR)
+        for bkup_dir in backup_dir:
+            passfile = open(BACKUPDIR + bkup_dir +
+                            "/398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b", "r")
+            line_list = passfile.readlines()
+            secret64 = line_list[6][1:29]
+            salt64 = line_list[10][1:9]
+            crack(secret64, salt64)
+    except OSError as e:
         secret64 = raw_input("Enter Secret Key: ")
         if secret64 < 3:
             secret64 = NONE
-    while not salt64:
         salt64 = raw_input("Enter Salt: ")
         if salt64 < 10:
             salt64 = NONE
-    crack(secret64, salt64)
+        crack(secret64, salt64)
+
+
+if __name__ == "__main__":
+    getHash()
