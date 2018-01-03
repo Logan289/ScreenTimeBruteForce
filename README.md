7d0ad63bf566de9b4430f36363fbbcbb307294ff
Aidan Holland <thehappydinoa@gmail.com>
2018-01-03 17:00:07 -0500

    Style Fixes

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # iOS Restriction Passcode Brute Force [![Python2.7](https://img.shields.io/badge/Python-2.7-green.svg?style=flat-square)](https://www.python.org/downloads/release/python-2714/)
 
-
 ## Overview
 
 This version of the application is written in Python, which is used to crack the restriction passcode of an iPhone/iPad takes advantage of a flaw in unencrypted backups allowing the hash and salt to be discovered.
@@ -15,20 +14,20 @@ This version of the application is written in Python, which is used to crack the
 
 3. Try from 1 to 9999 to with the pbkdf2-hmac-sha1 hash with Passlib
 
-
 ## How to Use
 
 1. Make sure to use iTunes to backup the iOS device to computer
 
 2. Run ioscrack.py
-`python ioscrack.py`
+```bash
+python ioscrack.py
+```
 
 ## DEPENDENCIES
 
 This has been tested with Python 2.6 and 2.7.
 
-Requires Passlib
-Install with `pip install passlib`
+Requires Passlib 1.7 Install with `pip install passlib`
 
 ## LICENSE
 
--- ioscrack.py
+++ ioscrack.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-#Filename: ioscrack.py
+# Filename: ioscrack.py
 
 from passlib.utils.pbkdf2 import pbkdf2
 from time import time
-import os, sys, base64
+import os
+import sys
+import base64
 
 HOMEDIR = '~/Library/Application Support/MobileSync/Backup/'
+
 
 def crack(secret64, salt64):
     print "secret: ", secret64
@@ -14,19 +17,21 @@ def crack(secret64, salt64):
     salt = base64.b64decode(salt64)
     start_t = time()
     for i in range(10000):
-        key = "%04d" % ( i )
+        key = "%04d" % (i)
         out = pbkdf2(key, salt, 1000)
         if out == secret:
             print "key: ", key
             duration = time() - start_t
-            print "%f seconds" % ( duration )
+            print "%f seconds" % (duration)
             sys.exit(0)
     print "no exact key"
+
 
 try:
     backup_dir = os.listdir(HOMEDIR)
     for bkup_dir in backup_dir:
-        passfile = open(HOMEDIR + bkup_dir + "/398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b", "r")
+        passfile = open(HOMEDIR + bkup_dir +
+                        "/398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b", "r")
         line_list = passfile.readlines()
         secret64 = line_list[6][1:29]
         salt64 = line_list[10][1:9]
